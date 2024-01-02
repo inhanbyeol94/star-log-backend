abbaac9062b9831c9ceebe9664933d36f0030b1c
inhanbyeol <inhanbyeol94@gmail.com>
2024-01-03 00:06:07 +0900

    init: ConfigModule 설정
    - isGlobal : true

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MemberModule } from './member/member.module';
+import { JwtModule } from './_common/jwt/jwt.module';
+import { BcryptModule } from './_common/bcrypt/bcrypt.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './_common/prisma/prisma.module';
+import { RedisModule } from './_common/redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MemberModule, JwtModule, BcryptModule, PrismaModule, RedisModule],
   controllers: [],
   providers: [],
 })
