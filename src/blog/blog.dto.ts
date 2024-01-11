d0796beaa1b92ca78b574a726cb100857874e62f
shimjaedoo <woen1637@naver.com>
2024-01-11 14:09:33 +0900

    feat: 블로그 생성/수정 DTO

---
+++ src/blog/blog.dto.ts
@@ -0,0 +1,52 @@
+import { IsNotEmpty, IsNumber, IsString, Matches, MaxLength, MinLength } from '@inhanbyeol/class-validator';
+import { blogRegex } from './blog.regex';
+
+export class CreateBlogDto {
+  @IsNotEmpty()
+  @IsNumber()
+  memberId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  @MinLength(10)
+  @Matches(blogRegex.address)
+  address: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
+  @MinLength(1)
+  @Matches(blogRegex.title)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @MinLength(2)
+  @Matches(blogRegex.description)
+  description: string;
+}
+
+export class UpdateBlogDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  @MinLength(10)
+  @Matches(blogRegex.address)
+  address: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
+  @MinLength(1)
+  @Matches(blogRegex.title)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @MinLength(2)
+  @Matches(blogRegex.description)
+  description: string;
+}
