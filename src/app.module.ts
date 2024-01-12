33bf86d13ed97f3cd6f095f72c2efd09b1710954
inhanbyeol <inhanbyeol94@gmail.com>
2024-01-12 22:53:13 +0900

    style: .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { JwtModule } from './_common/jwt/jwt.module';
 import { BcryptModule } from './_common/bcrypt/bcrypt.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './_common/prisma/prisma.module';
 import { RedisModule } from './_common/redis/redis.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-yet';
 import { LoggerModule } from './_common/logger/logger.module';
 import { AuthModule } from './auth/auth.module';
 
