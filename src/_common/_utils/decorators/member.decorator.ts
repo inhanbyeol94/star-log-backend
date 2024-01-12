86eb9d547d7e70e7bd3e9b66d200fd24c0e06cf4
inhanbyeol <inhanbyeol94@gmail.com>
2024-01-12 22:53:44 +0900

    feat: member 데코레이터 추가

--- src/_common/_utils/decorators/member.decorator.ts
+++ src/_common/_utils/decorators/member.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { IRequest } from '../interfaces/request.interface';
 
 export const Member = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
+  const request: IRequest = ctx.switchToHttp().getRequest();
+  return request.member;
 });
