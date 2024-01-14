da0b62bd64e4cc542280f2eadcb87421f83377a2
inhanbyeol <inhanbyeol94@gmail.com>
2024-01-14 22:20:42 +0900

    refactor: 네이밍 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Ip, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Ip, UseGuards } from '@nestjs/common';
 import { NaverAuthGuard } from '../_common/_utils/guards/naver.oauth.guard';
 import { Social } from '../_common/_utils/decorators/social.decorator';
 import { ISocial } from './auth.interface';
 import { AuthService } from './auth.service';
+import { Member } from '../_common/_utils/decorators/member.decorator';
+import { IPayload } from '../_common/jwt/jwt.interface';
+import { AccessToken } from '../_common/_utils/decorators/access-token.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Delete()
+  async logout(@Member() member: IPayload, @AccessToken() accessToken: string) {
+    return await this.authService.logout(member.id, accessToken);
+  }
+
   @Get('naver')
   @UseGuards(NaverAuthGuard)
   async naver(@Social() social: ISocial, @Ip() ip: string) {
