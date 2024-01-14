8f98dbd10a39339003781ca42687ee3e82e9523a
jeeeunu <dbwldms25@gmail.com>
2024-01-15 00:32:40 +0900

    feat: google.oauth.guard 추가

---
+++ src/_common/_utils/guards/google.oauth.guard.ts
@@ -0,0 +1,69 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import { IRequest } from '../interfaces/request.interface';
+import { platform } from '@prisma/client';
+
+@Injectable()
+export class GoogleAuthGuard implements CanActivate {
+  constructor(private configService: ConfigService) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request: IRequest = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(req: IRequest): Promise<any> {
+    const code = req.query.code as string;
+    console.log('code', code);
+    if (!code) return false;
+
+    try {
+      const accessToken = await this.getOauthToken(code);
+      console.log('accessToken', accessToken);
+      const profile = await this.getProfile(accessToken);
+      console.log('payload', profile);
+
+      if (!profile) return false;
+
+      const id = String(profile.id) || null;
+      const profileImage = profile.picture || null;
+
+      console.log('id,profileImage: ', id, profileImage);
+
+      if (!id || !profileImage) return false;
+
+      req.social = { id, profileImage, platform: platform.GOOGLE };
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async getOauthToken(code: string) {
+    const { data } = await axios.post(
+      'https://oauth2.googleapis.com/token',
+      {
+        grant_type: 'authorization_code',
+        client_id: this.configService.get('GOOGLE_CLIENT_ID'),
+        redirect_uri: this.configService.get('GOOGLE_CALLBACK_URL'),
+        client_secret: this.configService.get('GOOGLE_CLIENT_SECRET'),
+        state: 'starlog',
+        code,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          charset: 'UTF-8',
+        },
+      },
+    );
+    return data.access_token;
+  }
+
+  async getProfile(accessToken: string) {
+    const { data } = await axios.get(`https://www.googleapis.com/userinfo/v2/me?access_token=${accessToken}`);
+    return data;
+  }
+}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { NaverAuthGuard } from '../_common/_utils/guards/naver.oauth.guard';
 import { Social } from '../_common/_utils/decorators/social.decorator';
 import { ISocial } from './auth.interface';
 import { AuthService } from './auth.service';
+import { GoogleAuthGuard } from '../_common/_utils/guards/google.oauth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +11,12 @@ export class AuthController {
   @Get('naver')
   @UseGuards(NaverAuthGuard)
   async naver(@Social() social: ISocial, @Ip() ip: string) {
+    return await this.authService.oAuthLogin(social, ip);
+  }
+
+  @Get('google')
+  @UseGuards(GoogleAuthGuard)
+  async google(@Social() social: ISocial, @Ip() ip: string): Promise<string> {
     return await this.authService.oAuthLogin(social, ip);
   }
 }
