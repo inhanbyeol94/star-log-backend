4fa7c5b375f8e010d4c456adf9a916ea5fdefebc
inhanbyeol <inhanbyeol94@gmail.com>
2024-01-14 22:23:11 +0900

    refactor: 네이밍 수정

--- src/member/baned-member/baned-member.repository.ts
+++ src/member/baned-member/baned-member.repository.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../_common/prisma/prisma.service';
-import { BanedMember } from '@prisma/client';
+import { BannedMember } from '@prisma/client';
 
 @Injectable()
 export class BanedMemberRepository {
   constructor(private prisma: PrismaService) {}
 
-  private banedMemberRepository = this.prisma.extendedClient.banedMember;
+  private bannedMemberRepository = this.prisma.extendedClient.bannedMember;
 
   async create(memberId: string, reason: string, limitedAt: Date): Promise<void> {
-    await this.banedMemberRepository.create({
+    await this.bannedMemberRepository.create({
       data: {
         memberId,
         reason,
@@ -19,15 +19,15 @@ export class BanedMemberRepository {
   }
 
   async softDelete(memberId: number): Promise<void> {
-    await this.banedMemberRepository.delete({
+    await this.bannedMemberRepository.delete({
       where: {
         id: memberId,
       },
     });
   }
 
-  async findManyByMemberId(memberId: string): Promise<BanedMember[]> {
-    return this.banedMemberRepository.findMany({
+  async findManyByMemberId(memberId: string): Promise<BannedMember[]> {
+    return this.bannedMemberRepository.findMany({
       where: {
         memberId: memberId,
       },
