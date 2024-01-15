15714dd5dd2e938f721249b16e3a4c86df0a9763
shimjaedoo <woen1637@naver.com>
2024-01-15 22:28:51 +0900

    refactor: naming 수정

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Param, Patch, Post, Get } from '@nestjs/common';
 import { BlogService } from './blog.service';
-import { CreateBlogDto, UpdateBlogDto } from './blog.dto';
+import { BlogAddressDto, BlogIdDto, CreateBlogDto, UpdateBlogDto } from './blog.dto';
 import { Blog } from '@prisma/client';
 import { Member } from '../_common/_utils/decorators/member.decorator';
 import { IPayload } from '../_common/jwt/jwt.interface';
@@ -8,22 +8,22 @@ import { IPayload } from '../_common/jwt/jwt.interface';
 /**
  * Blog 관련 요청을 처리하는 Controller Class
  */
-@Controller('blog')
+@Controller('blogs')
 export class BlogController {
   constructor(private blogService: BlogService) {}
 
   /* 블로그 생성 */
   @Post()
   //todo 가드추가 예정
-  async create(@Member() member: IPayload, @Body() data: CreateBlogDto): Promise<string> {
-    return await this.blogService.create(member.id, data);
+  async create(@Member() member: IPayload, @Body() body: CreateBlogDto): Promise<string> {
+    return await this.blogService.create(member.id, body);
   }
 
   /* 블로그 수정 */
   @Patch(':id')
   //todo 가드추가 예정
-  async update(@Param('id') id: number, @Body() data: UpdateBlogDto): Promise<string> {
-    return await this.blogService.update(id, data);
+  async update(@Member() member: IPayload, @Param() param: BlogIdDto, @Body() body: UpdateBlogDto): Promise<string> {
+    return await this.blogService.update(param.id, member.id, body);
   }
 
   /* 블로그 전체조회 */
@@ -36,21 +36,21 @@ export class BlogController {
   /* 블로그 ID별 조회 */
   @Get(':id')
   //todo 가드추가 예정
-  async findOneById(@Param('id') id: number): Promise<Blog> {
-    return await this.blogService.findOneById(id);
+  async findFirstById(@Param() param: BlogIdDto): Promise<Blog> {
+    return await this.blogService.findUnique(param.id);
   }
 
   /* 블로그 주소별 조회 */
   @Get('/address/:address')
   //todo 가드추가 예정
-  async findByAddress(@Param('address') address: string): Promise<Blog> {
-    return await this.blogService.findByAddress(address);
+  async findFirstByAddress(@Param() param: BlogAddressDto): Promise<Blog> {
+    return await this.blogService.findFirstByAddress(param.address);
   }
 
   /* 블로그 삭제 */
   @Delete(':id')
   //todo 가드추가 예정
-  async delete(@Param('id') id: number): Promise<string> {
-    return await this.blogService.delete(id);
+  async delete(@Member() member: IPayload, @Param() param: BlogIdDto): Promise<string> {
+    return await this.blogService.softDelete(param.id, member.id);
   }
 }
