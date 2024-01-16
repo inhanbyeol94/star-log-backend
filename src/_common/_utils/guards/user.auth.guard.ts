ccbf83f7020d3ea24bd1de7c2d6f0b490ff3eaf7
인한별 <inhanbyeol94@limefriends.com>
2024-01-16 17:57:19 +0900

    feat: User 인증 가드 구현

---
+++ src/_common/_utils/guards/user.auth.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import { IRequest } from '../interfaces/request.interface';
+import { JwtService } from '../../jwt/jwt.service';
+import { RedisService } from '../../redis/redis.service';
+
+@Injectable()
+export class UserAuthGuard implements CanActivate {
+  constructor(
+    private configService: ConfigService,
+    private jwtService: JwtService,
+    private redisService: RedisService,
+  ) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request: IRequest = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(req: IRequest): Promise<boolean> {
+    const accessToken = req.headers.authorization;
+    if (!accessToken) return false;
+
+    const payload = this.jwtService.verify(accessToken);
+    if (typeof payload === 'string') throw new UnauthorizedException(payload);
+    const sessions = await this.redisService.findFirstByAccessToken(payload.id);
+    if (!sessions.includes(accessToken)) return false;
+
+    req.member = payload;
+
+    return true;
+  }
+}
