3d12b08b7ca98826df56a552932392fc746c2db5
inhanbyeol <inhanbyeol94@gmail.com>
2024-01-23 00:19:25 +0900

    feat: tag CRUD 구현

--- src/blog/tag/tag.service.ts
+++ src/blog/tag/tag.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { TagRepository } from './tag.repository';
+import { Prisma, Tag } from '@prisma/client';
 
 @Injectable()
 export class TagService {
   constructor(private tagRepository: TagRepository) {}
+
+  async create(data: Prisma.TagCreateInput): Promise<Tag> {
+    return await this.tagRepository.create(data);
+  }
+
+  async update(id: number, data: Prisma.TagUpdateInput): Promise<Tag> {
+    await this.tagRepository.findUniqueOrThrow(id);
+    return await this.tagRepository.update(id, data);
+  }
+
+  async softDelete(id: number): Promise<void> {
+    await this.tagRepository.findUniqueOrThrow(id);
+    return await this.tagRepository.softDelete(id);
+  }
+
+  async findUnique(id: number): Promise<Tag> {
+    return await this.tagRepository.findUniqueOrThrow(id);
+  }
 }
