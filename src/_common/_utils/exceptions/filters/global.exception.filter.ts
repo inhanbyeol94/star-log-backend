19e90d2bb573169a8389dec39ec76e74cd762676
인한별 <inhanbyeol94@limefriends.com>
2024-01-26 11:21:48 +0900

    fix: Cannot GET /favicon.ico 오류 무시

--- src/_common/_utils/exceptions/filters/global.exception.filter.ts
+++ src/_common/_utils/exceptions/filters/global.exception.filter.ts
@@ -1,7 +1,7 @@
 import { Catch, ExceptionFilter, ArgumentsHost, HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
 import { logger } from '../../../logger/logger.service';
 
-@Catch(HttpException, Error)
+@Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
@@ -14,18 +14,11 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
     const message = exception instanceof HttpException ? exception.message : '서버 내부 오류로 처리할 수 없습니다.';
 
-    logger.error(`error: ${exception.message}, stack: ${exception.stack}`);
+    if (exception.message !== 'Cannot GET /favicon.ico') logger.error(`error: ${exception.message}, stack: ${exception.stack}`);
 
-    try {
-      response.status(status).json({
-        statusCode: status,
-        message: message,
-      });
-    } catch (error) {
-      response.status(500).json({
-        statusCode: 500,
-        message: '알 수 없는 오류가 발생하였습니다.',
-      });
-    }
+    response.status(status).json({
+      statusCode: status,
+      message: message,
+    });
   }
 }
