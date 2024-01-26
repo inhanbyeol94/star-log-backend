cc66a30d77178bce24e661e4f2eca5e931bd9e99
shimjaedoo <woen1637@naver.com>
2024-01-26 15:47:28 +0900

    modify: 경로추가 및 가드설정

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,27 +1,28 @@
-import { Body, Controller, Delete, Get, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Put, UseGuards } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { Member } from 'src/_common/_utils/decorators/member.decorator';
 import { IPayload } from '../_common/jwt/types/payload.interface';
 import { MemberUpdateDto } from './types/update/request.dto';
+import { UserAuthGuard } from '../_common/_utils/guards/user.auth.guard';
 
 @Controller('member')
 export class MemberController {
   constructor(private memberService: MemberService) {}
 
-  @Put()
-  //todo guard 추가예정
+  @Put('me')
+  @UseGuards(UserAuthGuard)
   async update(@Member() member: IPayload, @Body() body: MemberUpdateDto) {
     return await this.memberService.update(member.id, body);
   }
 
-  @Delete()
-  //todo guard 추가예정
+  @Delete('me')
+  @UseGuards(UserAuthGuard)
   async softDelete(@Member() member: IPayload) {
     return await this.memberService.softDelete(member.id);
   }
 
-  @Get()
-  //todo guard 추가예정
+  @Get('me')
+  @UseGuards(UserAuthGuard)
   async findUnique(@Member() member: IPayload) {
     return await this.memberService.findUnique(member.id);
   }
